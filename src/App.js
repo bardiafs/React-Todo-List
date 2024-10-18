8f981be1831cd767979bb673f4729f1676f1f9ff
bardiafar <bardiafarokhsereshti@gmail.com>
2024-10-19 00:41:18 +0330

    adding buttons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@
 import React from 'react';
 import './styles.css';
 
+function Button({text, type}){
+  return (
+    <button className={type}>{text}</button>
+  )
+}
+
 function App(){
   return(
-    <h1>TODO LIST
-    </h1>
+    <>
+    <h1>TODO LIST</h1>
+    <div class="division">
+      <Button text="Search" type="darkButton"/>
+      <Button text="New Task" type="brightButton"/>
+    </div>
+    <div class="division">
+      <input type="text" placeholder="Search for tasks..." />
+      <Button text="Go" type="brightButton"/>
+    </div>
+    <div class="division">
+      <span>Filters:   </span>
+      <Button text="Done" type="darkButton"/>
+      <Button text="in Progress" type="darkButton"/>
+      <Button text="All" type="darkButton"/>
+    </div>
+    </>
   );
 };
 
